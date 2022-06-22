d4a277edc10023db48c4c2515b404e69c85c2f12
abalkibekov <a.balkibekov@gmail.com>
2022-06-22 13:58:43 +0400

    refactor: replace switch by arrays

--- src/index.js
+++ src/index.js
@@ -14,6 +14,11 @@ module.exports = function toReadable (number) {
     return resultString.trim();
 }
 
+const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+const tens_10_19 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+const numericClasses = ['', 'thousand', 'million', 'billion', 'trillion'];
+
 function splitToStringsByNumericClasses(number){
     let numberStr = '' + number;
     let numberOfNumericClasses = ~~(numberStr.length / 3);
@@ -34,7 +39,7 @@ function numbersToReadble(numbersString){
     }
     if (numbersString.length === 2){
         if (numbersString[0] === '1'){
-            return to_10_19_Names(+numbersString);
+            return to_10_19_Names(+numbersString[1]);
         }
         let tensDesc = toTensName(+numbersString[0]);
         let onesDesc = toOnesName(+numbersString[1]);
@@ -42,142 +47,25 @@ function numbersToReadble(numbersString){
     }
     let hundreds = toOnesName(+numbersString[0]) +' hundred';
     if (numbersString[1] === '1'){
-        return hundreds + ' ' + to_10_19_Names(+numbersString.slice(-2));
+        return hundreds + ' ' + to_10_19_Names(+numbersString[2]);
     }
     let tens = toTensName(+numbersString[1]);
     let onesDesc = toOnesName(+numbersString[2]);
     return `${hundreds}${tens ? ' ' + tens : ''}${onesDesc ? ' ' + onesDesc : ''}`;
 }
 
-function numericClassToReadable(numericClass){
-    let classDescription;
-    switch (numericClass) {
-        case 1:
-            classDescription = 'thousand';
-            break;
-        case 2:
-            classDescription = 'million';
-            break;
-        case 3:
-            classDescription = 'billion';
-            break;
-        case 4:
-            classDescription = 'trillion';
-            break;
-        default:
-            classDescription = '';
-            break;
-    }
-    return classDescription;
+function numericClassToReadable(digit){
+    return digit < 0 || digit > 4 ? '' : numericClasses[digit];
 };
 
 function toOnesName(digit){
-    let name = '';
-    switch (digit) {
-        case 1:
-            name = 'one';
-            break;
-        case 2:
-            name = 'two';
-            break;
-        case 3:
-            name = 'three';
-            break;
-        case 4:
-            name = 'four';
-            break;
-        case 5:
-            name = 'five';
-            break;
-        case 6:
-            name = 'six';
-            break;
-        case 7:
-            name = 'seven';
-            break;
-        case 8:
-            name = 'eight';
-            break;
-        case 9:
-            name = 'nine';
-            break;
-        default:
-            name = '';
-            break;
-    }
-    return name;
+    return digit < 0 || digit > 9 ? '' : ones[digit];
 }
 
 function to_10_19_Names(digit) {
-    let name;
-    switch (digit) {
-        case 10:
-            name = 'ten';
-            break;
-        case 11:
-            name = 'eleven';
-            break;
-        case 12:
-            name = 'twelve';
-            break;
-        case 13:
-            name = 'thirteen';
-            break;
-        case 14:
-            name = 'fourteen';
-            break;
-        case 15:
-            name = 'fifteen';
-            break;
-        case 16:
-            name = 'sixteen';
-            break;
-        case 17:
-            name = 'seventeen';
-            break;
-        case 18:
-            name = 'eighteen';
-            break;
-        case 19:
-            name = 'nineteen';
-            break;
-        default:
-            name = '';
-            break;
-    }
-    return name;
+    return digit < 0 || digit > 9 ? '' : tens_10_19[digit];
 }
 
 function toTensName(digit){
-    let name = '';
-    switch (digit) {
-        case 2:
-            name = 'twenty';
-            break;
-        case 3:
-            name = 'thirty';
-            break;
-        case 4:
-            name = 'forty';
-            break;
-        case 5:
-            name = 'fifty';
-            break;
-        case 6:
-            name = 'sixty';
-            break;
-        case 7:
-            name = 'seventy';
-            break;
-        case 8:
-            name = 'eighty';
-            break;
-        case 9:
-            name = 'ninety';
-            break;
-        default:
-            name = '';
-            break;
-    }
-    return name;
+    return digit < 0 || digit > 9 ? '' : tens[digit];
 }
